e9a4a2b66136f4cf8684dc451e1beff4b169eddd
Paul Pham <ppham@Pauls-MacBook-Pro.local>
2018-02-02 18:49:22 -0500

    initial creation of commentbox js files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// index.js
+import React from 'react';
+import ReactDom from 'react';
+import CommentBox from './CommentBox'
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('root')
+);
